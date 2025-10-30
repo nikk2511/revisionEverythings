a597fcf60b8d3ad9ca6287f2fdfc628cfcd596a2
nikk2511 <nikhil95696@gmail.com>
2025-10-30 13:51:46 +0530

    first commit

---
+++ 01/test2.js
@@ -0,0 +1,41 @@
+"use strict"; // treat all Js code as newer version
+
+// console.log("This is test2.js file");
+
+// console.log(3+4);
+
+
+let age = 19;
+
+let isLogedIn = false;
+
+let state = null;
+
+// number
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Symbol
+
+
+
+// Object
+
+let ages = 18;
+
+
+console.log(typeof null)
+console.log(typeof age)
+
+console.log(typeof undefined)
+
+
+
+
+
+
+
+
+
