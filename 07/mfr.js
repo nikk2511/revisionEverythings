23e042017ea1e217ff1d826995df8b4a065cf490
nikk2511 <nikhil95696@gmail.com>
2025-11-18 23:46:44 +0530

    chore: add initial implementation of various array methods and user data

---
+++ 07/mfr.js
@@ -0,0 +1,121 @@
+// const arr = [5,1,3,2,6];
+
+
+
+// // function double(x) {
+// //     return x * 2;
+// // }
+
+// // function triple(x) {
+// //     return x*x*x;
+// // }
+
+// // function binary(x) {
+// //     return x.toString(2);
+// // }
+
+// const output = arr.map((x) => x.toString(2));
+
+
+// // const output = arr.map(binary);
+
+// console.log(output);
+
+
+// const arr = [5,1,3,2,6];
+
+// // filter odd value
+// function isOdd(x) {
+//     return x % 2;
+// }
+
+// const output = arr.filter(isOdd);
+
+// console.log(output);
+
+
+
+// const arr = [5,1,3,2,6];
+
+// // filter odd value
+// function isEven(x) {
+//     return x % 2 === 0;
+// }
+
+// const output = arr.filter(isEven);
+
+// console.log(output);
+
+
+// const arr = [5,1,3,2,6];
+
+// // sum or max
+
+// function findMax (arr) {
+//     let max = 0;
+//     for (let i =0; i<arr.length; i++) {
+//         if(arr[i] > max) {
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+
+// console.log(findMax(arr));
+
+
+// // const output = arr.reduce(function(acc, curr) {
+// //     acc= acc + curr;
+
+// //     return acc;
+// // }, 0);
+// // console.log(output);
+
+
+
+// const output = arr.reduce(function(max, curr) {
+//     if (curr > max) {
+//         max = curr;
+//     }
+//     return max;
+// }, 0)
+
+
+
+
+const users = [
+
+{ firstName: "akshay", lastName: "saini", age: 26 },
+{ firstName: "donald", lastName: "trump", age: 75 },
+{ firstName: "elon", lastName: "musk", age: 50 },
+{ firstName: "deepika", lastName: "padukone", age: 26 },
+
+];
+
+// list of fullName 
+
+// const output = users.map(x =>x.firstName + " " + x.lastName);
+// console.log(output);
+
+// const output = users.reduce(function(acc, curr) {
+//     if(acc[curr.age]) {
+//         acc[curr.age] = ++acc[curr.age];
+//     } else {
+//         acc[curr.age] = 1;
+//     }
+//     return acc;
+// }, {});
+
+// console.log(output)
+
+
+
+var a;
+console.log(a);
+
+
+
+   
+
+
+
