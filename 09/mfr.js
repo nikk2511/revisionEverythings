f5c53571aeca668a36045a50cac5ff64c21522b3
nikk2511 <nikhil95696@gmail.com>
2025-11-26 23:56:23 +0530

    chore: add initial implementations of filter and includes methods in mfr.js and test.js

---
+++ 09/mfr.js
@@ -0,0 +1,15 @@
+// const coding = ['js', 'py', 'java', 'cpp', 'rb', 'go', 'rs', 'ts', 'cs', 'php'];
+
+// const value = coding.forEach( (item) => {
+//   console.log(item);
+//   return item;
+// } );
+
+// console.log(value); 
+
+const myNums = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+const newNums = myNums.filter ( (num) => num > 40 )
+
+console.log(newNums);
+
---
+++ 09/test.js
@@ -0,0 +1,17 @@
+// function func1(){
+// return 2
+// }
+// function func2(){
+// return 4
+// }
+// let a = (func1(), func2())
+
+// console.log(a);
+
+const arr = ['one', 'two', 'three'];
+const str = 'Hello';
+
+const res = arr.includes('on')
+const anotherVar = arr.includes('onetwo')
+const newVar = str.includes('ll')
+
